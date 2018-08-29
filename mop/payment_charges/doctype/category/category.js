91c55a8e11417834391ee8e848fd8d410ae8dd53
ERpNext <erpdev@ubuntu-2gb-lon1-01.localdomain>
2018-08-29 11:17:37 +0000

    importing all customizations -fixtures and docs

---
+++ mop/payment_charges/doctype/category/category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, taherkhalil and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('category', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ mop/payment_charges/doctype/subcategory/subcategory.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, taherkhalil and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('subcategory', {
+	refresh: function(frm) {
+
+	}
+});
